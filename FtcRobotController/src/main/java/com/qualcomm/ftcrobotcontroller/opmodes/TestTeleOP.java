ad48e7510cf2a1c1f378f6497347ac468bd50fdd
WHSRobotics4 <WilsonRobots@gmail.com>
2015-12-05 14:13:08 -0800

    TeleOp!

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestTeleOP.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TestTeleOP.java
@@ -1,8 +1,6 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
         import com.qualcomm.robotcore.eventloop.opmode.OpMode;
         import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.hardware.Servo;
-        import com.qualcomm.robotcore.util.Range;
 
 /**
  * Created by Robotics on 10/28/2015.
@@ -13,38 +11,32 @@ public class TestTeleOP extends OpMode {
     DcMotor frontRightMotor;
     DcMotor backLeftMotor;
     DcMotor backRightMotor;
+    DcMotor bucketMotor;
+    DcMotor tapeMotor;
+    DcMotor hookMotor;
+    DcMotor sweepMotor;
 
     private static final String frontLeft =  "front_left";  //motor name defines
     private static final String frontRight = "front_right";
     private static final String backLeft = "back_left";
     private static final String backRight = "back_right";
-    private static final String armName = "arm";
-    private static final String clawName = "claw";
+    private static final String bucketName = "bucket";
+    private static final String tapeName = "tape";
+    private static final String hookName = "hook";
+    private static final String sweepName = "sweep";
 
-    //private static final double armRest = 1.0;
-    //private static final double clawRest = 0.7;
-    //private static final double armStage1 = 0.8;
-    //private static final double armStage2 = 0.3;
-    //private static final double clawStage1 = 0.2;
-    private static final double frontMotorMultiple = 0.75;
+    private static final double frontMotorMultiple = 1.0;
     private static final double backMotorMultiple = 1.0;
+    private static final double triggerThresh = 0.05;
 
-    Servo armBase;
-    Servo armPrimary1;
-    Servo armPrimary2;
-    //Servo armSecondary1;
-    //Servo armSecondary2;
-    //Servo armHatch;
+    private static final double hookPowerUp = 1;
+    private static final double hookPowerDown = -1;
 
-    private static final String baseName = "base";
-    private static final String primary1Name = "primary_1";
-    private static final String primary2Name = "primary_2";
-    //private static final String secondary1Name = "secondary_1";
-    //private static final String secondary2Name = "secondary_2";
-    //private static final String hatchName = "hatch";
+    private static final double bucketPowerUp = 0.2;
+    private static final double bucketPowerDown = -0.2;
 
-    //private boolean lastTriggerState = false;
-    //private boolean armLockState = false;
+    private static final double sweepPowerUp = 1.0;
+    private static final double sweepPowerDown = -1.0;
 
     //constructor
     public TestTeleOP() {
@@ -59,30 +51,23 @@ public class TestTeleOP extends OpMode {
         frontRightMotor = hardwareMap.dcMotor.get(frontRight);
         backRightMotor = hardwareMap.dcMotor.get(backLeft);
         backLeftMotor = hardwareMap.dcMotor.get(backRight);
+        bucketMotor = hardwareMap.dcMotor.get(bucketName);
+        tapeMotor = hardwareMap.dcMotor.get(tapeName);
+        hookMotor = hardwareMap.dcMotor.get(hookName);
+        sweepMotor = hardwareMap.dcMotor.get(sweepName);
 
         frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
         //backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         //frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        backRightMotor.setDirection((DcMotor.Direction.REVERSE));
+        backRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        tapeMotor.setDirection(DcMotor.Direction.REVERSE);
+        hookMotor.setDirection(DcMotor.Direction.REVERSE);
         //arm = hardwareMap.servo.get("servo_1");
         //claw = hardwareMap.servo.get("servo_6");
 
         // set the gamepad 2 dead zone to 0
         gamepad2.setJoystickDeadzone(0.0f);
 
-        // robotic arm servos
-        armBase = hardwareMap.servo.get(baseName);
-        armPrimary1 = hardwareMap.servo.get(primary1Name);
-        armPrimary2 = hardwareMap.servo.get(primary2Name);
-        //armSecondary1 = hardwareMap.servo.get(secondary1Name);
-        //armSecondary2 = hardwareMap.servo.get(secondary2Name);
-        //armHatch = hardwareMap.servo.get(hatchName);
-
-        armPrimary1.setPosition(0.5);
-        armPrimary2.setPosition(0.5);
-        //armSecondary1.setPosition(0.5);
-        armBase.setPosition(0.5);
-        //armHatch.setPosition(1.0);
     }
 
     @Override
@@ -114,7 +99,22 @@ public class TestTeleOP extends OpMode {
         frontLeftMotor.setPower(leftThrottle * frontMotorMultiple);
         backRightMotor.setPower(rightThrottle * backMotorMultiple);
         frontRightMotor.setPower(rightThrottle * frontMotorMultiple);
-        // update the position of the arm
+        // update the position of everything else
+        if(gamepad2.left_trigger > triggerThresh) tapeMotor.setPower(gamepad2.left_trigger);
+        else if(gamepad2.right_trigger > triggerThresh) tapeMotor.setPower(-gamepad2.right_trigger);
+        else tapeMotor.setPower(0);
+
+        if(gamepad2.right_bumper) hookMotor.setPower(hookPowerDown);
+        else if(gamepad2.left_bumper) hookMotor.setPower(hookPowerUp);
+        else hookMotor.setPower(0);
+
+        if(gamepad2.a) bucketMotor.setPower(bucketPowerUp);
+        else if (gamepad2.b) bucketMotor.setPower(bucketPowerDown);
+        else bucketMotor.setPower(0);
+
+        if(gamepad1.left_bumper)  sweepMotor.setPower(sweepPowerUp);
+        else if (gamepad1.right_bumper) sweepMotor.setPower(sweepPowerDown);
+        else sweepMotor.setPower(0);
 		/*
 		 * Send telemetry data back to driver station. Note that if we are using
 		 * a legacy NXT-compatible motor controller, then the getPower() method
@@ -128,10 +128,6 @@ public class TestTeleOP extends OpMode {
         telemetry.addData("right tgt pwr", "right pwr: " + String.format("%.2f", rightThrottle));
         telemetry.addData("Left Trigger", gamepad2.left_bumper);
         telemetry.addData("Right Trigger", gamepad2.right_bumper);
-
-        //boolean bumpers = gamepad2.left_bumper || gamepad2.right_bumper;
-        updateArmServos(gamepad2.left_stick_x, gamepad2.right_stick_x, gamepad2.left_trigger, gamepad2.right_trigger, gamepad2.a);
-        //lastTriggerState = triggers;
     }
 
     @Override
@@ -172,31 +168,5 @@ public class TestTeleOP extends OpMode {
 
         // return scaled value.
         return dScale;
-    }
-
-    public void updateArmServos(double joystickLeftX, double joystickRightX, double leftTrigger, double rightTrigger, boolean buttonA) {
-
-        // toggle lock
-        //if(triggers && !lastTriggerState) armLockState = !armLockState;
-
-        //if(!armLockState)
-        //{
-            // base control (triggers)
-            if(leftTrigger > 0.0) armBase.setPosition(1.0);
-            else if(rightTrigger > 0.0) armBase.setPosition(0.0);
-            else armBase.setPosition(0.5);
-
-            // primary joint control (left joystick)
-            armPrimary1.setPosition((joystickLeftX + 1.0) / 2.0);
-            armPrimary2.setPosition(1.0 - (joystickLeftX + 1.0) / 2.0);
-
-            // secondary joint control (right joystick)
-            //armSecondary1.setPosition((joystickRightX + 1.0) / 2.0);
-
-            // hatch control (A button)
-            //if(buttonA) armHatch.setPosition(1.0);
-            //else armHatch.setPosition(0.0);
-        //}
-        //else armBase.setPosition(0.5);
     }
 }
