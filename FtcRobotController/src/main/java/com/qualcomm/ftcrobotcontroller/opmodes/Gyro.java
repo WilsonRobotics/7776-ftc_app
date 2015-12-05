ac7298464e8792d1351734884a19ecc21eb580f8
WHSRobotics5 <WilsonRobots@gmail.com>
2015-12-04 17:42:45 -0800

    added new opmodes

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Gyro.java
@@ -0,0 +1,11 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * Created by Robotics on 12/4/2015.
+ */
+public class Gyro {
+
+    private static final int port = 5;
+
+
+}
---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/music.java
@@ -0,0 +1,7 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * Created by Robotics on 12/4/2015.
+ */
+public class music {
+}
