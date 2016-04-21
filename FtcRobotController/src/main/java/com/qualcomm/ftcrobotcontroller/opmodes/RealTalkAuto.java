2f169370a7e0da1625407335c93b7250588f010b
WHSRobotics5 <WilsonRobots@gmail.com>
2016-04-20 19:41:48 -0700

    I did thing whic I do not remember (reverted from commit cde28abd19651d8e2bb1ae2bdbb1662880f5c0dc)

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/RealTalkAuto.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/RealTalkAuto.java
@@ -5,13 +5,15 @@ import android.media.MediaPlayer;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
 
 
 /**
  * A test example of autonomous opmode programming using AutoLib classes.
  * Created by phanau on 12/14/15.
-*/
+ */
 
 
 public class RealTalkAuto extends OpMode {
@@ -28,7 +30,8 @@ public class RealTalkAuto extends OpMode {
     Servo armRelease2;
     Servo flag1;
     Servo flag2;
-    navXGyro mGyro;
+    ModernRoboticsI2cGyro mGyro1;   // gyro(s) to use for heading information
+    SensorLib.CorrectedMRGyro mCorrGyro;    // gyro corrector object
     SensorLib.PID mPid;                     // PID controller for the sequence
 
     boolean red = false;
@@ -47,8 +50,6 @@ public class RealTalkAuto extends OpMode {
     private static final String release2Name = "dropit2";
     private static final String flag1Name = "flag_left";
     private static final String flag2Name = "flag_right";
-    private static final String dimName = "dim";
-    private static final int dimPort = 0;
 
     private static final double forward_power = 1.0;
     private static final double mountain_idle_power = 0.1;
@@ -67,8 +68,8 @@ public class RealTalkAuto extends OpMode {
     private static final double flagPullPowerDown = 0.0;
 
     // parameters of the PID controller for this sequence
-    float Kp = 0.10f;        // motor power proportional term correction per degree of deviation
-    float Ki = 0.08f;         // ... integrator term
+    float Kp = 0.035f;        // motor power proportional term correction per degree of deviation
+    float Ki = 0.02f;         // ... integrator term
     float Kd = 0;             // ... derivative term
     float KiCutoff = 0.0f;    // maximum angle error for which we update integrator
 
@@ -81,8 +82,7 @@ public class RealTalkAuto extends OpMode {
         armRelease2 = hardwareMap.servo.get(release2Name);
         flag1 = hardwareMap.servo.get(flag1Name);
         flag2 = hardwareMap.servo.get(flag2Name);
-
-        mGyro = new navXGyro(hardwareMap.deviceInterfaceModule.get(dimName), dimPort);
+        mGyro1 = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get(gyro_name);
 
         front_right.setDirection(DcMotor.Direction.REVERSE);
         front_left.setDirection(DcMotor.Direction.FORWARD);
@@ -102,6 +102,8 @@ public class RealTalkAuto extends OpMode {
         mp = new MediaPlayer();
         emp = new MediaPlayer();
 
+        mCorrGyro = new SensorLib.CorrectedMRGyro(mGyro1);
+
         // create a PID controller for the sequence
         mPid = new SensorLib.PID(Kp, Ki, Kd, KiCutoff);    // make the object that implements PID control algorithm
 
@@ -114,21 +116,21 @@ public class RealTalkAuto extends OpMode {
         AutoLib.LinearSequence drivingToBox = new AutoLib.LinearSequence();
         //drivingToBox.add(new AutoLib.TurnByEncoder(front_right, null, front_left, null,
         //        right_forward_power, left_forward_power, 4500, 4500, true));
-        drivingToBox.add(new AutoLib.AzimuthCountedDriveStep(this, 0, mGyro, mPid, motors, (float)forward_power, 4500, true));
+        drivingToBox.add(new AutoLib.AzimuthCountedDriveStep(this, 0, mCorrGyro, mPid, motors, (float)forward_power, 4500, true));
         if(!red) drivingToBox.add(new AutoLib.TurnByEncoder(front_right, null, front_left, null,
                 -forward_power, forward_power, 1300, 1300, true));
         else drivingToBox.add(new AutoLib.TurnByEncoder(front_right, null, front_left, null,
                 forward_power, -forward_power, 1300, 1300, true));
         //drivingToBox.add(new AutoLib.TurnByEncoder(front_right, null, front_left, null,
         //        right_forward_power, left_forward_power, 9500, 9500, true));
-        drivingToBox.add(new AutoLib.AzimuthCountedDriveStep(this, 45, mGyro, mPid, motors, (float)forward_power, 8500, true));
+        drivingToBox.add(new AutoLib.AzimuthCountedDriveStep(this, 45, mCorrGyro, mPid, motors, (float)forward_power, 9500, true));
         if(!red) drivingToBox.add(new AutoLib.TurnByEncoder(front_right, null, front_left, null,
                 -forward_power, forward_power, 1300, 1300, true));
         else drivingToBox.add(new AutoLib.TurnByEncoder(front_right, null, front_left, null,
                 forward_power, -forward_power, 1300, 1300, true));
         //drivingToBox.add(new AutoLib.TurnByEncoder(front_right, null, front_left, null,
         //        right_forward_power/2, left_forward_power/2, 3000, 3000, true));
-        drivingToBox.add(new AutoLib.AzimuthCountedDriveStep(this, 90, mGyro, mPid, motors, (float)forward_power/4, 4000, true));
+        drivingToBox.add(new AutoLib.AzimuthCountedDriveStep(this, 90, mCorrGyro, mPid, motors, (float)forward_power/4, 4000, true));
         mainSequence.add(drivingToBox);
 
         AutoLib.ConcurrentSequence dropClimbers = new AutoLib.ConcurrentSequence();
@@ -145,11 +147,12 @@ public class RealTalkAuto extends OpMode {
 
         // start out not-done
         bDone = false;
+
     }
 
     @Override
     public void start(){
-        mGyro.calibrate();
+        mCorrGyro.calibrate();
     }
 
     public void loop() {
@@ -161,7 +164,32 @@ public class RealTalkAuto extends OpMode {
     public void stop() {
         front_left.setPower(0);
         front_right.setPower(0);
-        mGyro.stop();
         telemetry.addData("stop() called", "");
     }
 }
+
+
+/* for reference
+        // create the root Sequence for this autonomous OpMode
+        mSequence = new AutoLib.LinearSequence();
+
+        // add a first simple Step to the root Sequence
+        mSequence.add(new AutoLib.LogTimeStep(this, "step1", 10));
+
+        // create a ConcurrentSequence with 3 concurrent Steps
+        AutoLib.ConcurrentSequence cs1 = new AutoLib.ConcurrentSequence();
+            // step 1 of the 3 concurrent steps
+            cs1.add(new AutoLib.LogTimeStep(this, "step2a", 10));
+            // step 2 is itself a LinearSequence of two Steps
+            AutoLib.LinearSequence cs1a = new AutoLib.LinearSequence();
+                cs1a.add(new AutoLib.LogTimeStep(this, "step2b1", 6));
+                cs1a.add(new AutoLib.LogTimeStep(this, "step2b2", 9));
+            cs1.add(cs1a);
+            // step 3 is a simple Step
+            cs1.add(new AutoLib.LogTimeStep(this, "step2c", 5));
+        // add the ConcurrentSequence to the root Sequence
+        mSequence.add(cs1);
+
+        // finish up with another simple Step
+        mSequence.add(new AutoLib.LogTimeStep(this, "step3", 10));
+        */
